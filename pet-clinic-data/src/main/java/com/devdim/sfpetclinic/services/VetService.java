6fa337270c3daa6b9deb01fe6df524e7947be458
DimaPindrik <dyebolical@gmail.com>
2019-07-28 22:43:55 +0300

    Implement Interfaces for Services

---
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.devdim.sfpetclinic.services;
+
+import com.devdim.sfpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

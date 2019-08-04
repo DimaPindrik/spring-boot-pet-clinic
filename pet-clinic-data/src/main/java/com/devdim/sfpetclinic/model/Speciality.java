09d50bd7b89103adf2102e85041d3f388a2c98eb
DimaPindrik <dyebolical@gmail.com>
2019-08-04 22:06:25 +0300

    Adding PetType relation to Owner and Speciality added to Vet

---
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.devdim.sfpetclinic.model;
+
+/**
+ * created by Dima on 8/4/2019.
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

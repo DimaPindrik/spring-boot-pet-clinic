83f3defe38b22e37feeea8b86b31eadf21b3d291
DimaPindrik <dyebolical@gmail.com>
2019-08-04 22:14:48 +0300

    Adding PetType Service Map implementation

---
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.devdim.sfpetclinic.services;
+
+import com.devdim.sfpetclinic.model.PetType;
+
+/**
+ * created by Dima on 8/4/2019.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,39 @@
+package com.devdim.sfpetclinic.services.map;
+
+import com.devdim.sfpetclinic.model.PetType;
+import com.devdim.sfpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * created by Dima on 8/4/2019.
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}

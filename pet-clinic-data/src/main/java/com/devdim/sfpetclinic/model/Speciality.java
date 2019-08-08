a65896be0166f7bd212cdaefefb37155609bf6d9
DimaPindrik <dyebolical@gmail.com>
2019-08-08 17:36:42 +0300

    Converting Owner, Pet, Speciality, Vet and Visit to JPA entities

--- pet-clinic-data/src/main/java/com/devdim/sfpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package com.devdim.sfpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * created by Dima on 8/4/2019.
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {

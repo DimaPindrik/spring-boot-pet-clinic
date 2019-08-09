1100ebda382d24968d9f4eea9583c19be3a6ae2a
DimaPindrik <dyebolical@gmail.com>
2019-08-09 17:53:13 +0300

    Adding Visit Service and VisitServiceMap implementation

---
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.devdim.sfpetclinic.services;
+
+import com.devdim.sfpetclinic.model.Visit;
+
+/**
+ * created by Dima on 8/9/2019.
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/devdim/sfpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,44 @@
+package com.devdim.sfpetclinic.services.map;
+
+import com.devdim.sfpetclinic.model.Visit;
+import com.devdim.sfpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * created by Dima on 8/9/2019.
+ */
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
